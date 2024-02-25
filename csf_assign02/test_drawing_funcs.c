0206974a2a7b767865006e195360a8bd7104af46
lmalmud <lmalmud2@jh.edu>
2024-02-25 16:18:55 -0500

    implemented draw_rect (does not work)

--- csf_assign02/test_drawing_funcs.c
+++ csf_assign02/test_drawing_funcs.c
@@ -110,8 +110,8 @@ int main(int argc, char **argv) {
   TEST_INIT();
 
   // TODO: add TEST() directives for your helper functions
-  /*TEST(test_draw_rect);
-  TEST(test_draw_circle);
+  TEST(test_draw_rect);
+  /*TEST(test_draw_circle);
   TEST(test_draw_circle_clip);
   TEST(test_draw_tile);
   TEST(test_draw_sprite);*/
