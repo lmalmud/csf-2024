893a525bef7216fb7a9ffe4e45f27a64c82f89b2
lmalmud <lmalmud2@jh.edu>
2024-02-25 19:48:24 -0500

    working on draw_circle

--- csf_assign02/test_drawing_funcs.c
+++ csf_assign02/test_drawing_funcs.c
@@ -111,9 +111,9 @@ int main(int argc, char **argv) {
 
   // TODO: add TEST() directives for your helper functions
   TEST(test_draw_rect);
-  /*TEST(test_draw_circle);
+  TEST(test_draw_circle);
   TEST(test_draw_circle_clip);
-  TEST(test_draw_tile);
+  /*TEST(test_draw_tile);
   TEST(test_draw_sprite);*/
 
   // TESTS FOR HELPER FUNCTIONS
