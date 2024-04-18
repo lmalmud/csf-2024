dd487db4473e08c5dc52fc701365615416953cc6
bbock27 <bradybock11@icloud.com>
2024-04-18 17:23:49 -0400

    decode message half working

--- csf_assign05/message_serialization.cpp
+++ csf_assign05/message_serialization.cpp
@@ -85,5 +85,58 @@ void MessageSerialization::encode( const Message &msg, std::string &encoded_msg 
 
 void MessageSerialization::decode( const std::string &encoded_msg_, Message &msg )
 {
-  // TODO: implement
+  std::stringstream s(encoded_msg_);
+	std::string currArg("");
+
+	s >> currArg;
+
+	std::map<std::string,MessageType> stringToArg = {{"LOGIN", MessageType::LOGIN}, 
+	{"CREATE", MessageType::CREATE}, {"PUSH", MessageType::PUSH}, {"POP", MessageType::POP}, 
+	{"TOP", MessageType::TOP}, {"SET", MessageType::SET}, {"GET", MessageType::GET}, 
+	{"ADD", MessageType::ADD}, {"SUB", MessageType::SUB}, {"MUL", MessageType::MUL}, 
+	{"DIV", MessageType::DIV}, {"BEGIN", MessageType::BEGIN}, {"COMMIT", MessageType::COMMIT}, 
+	{"BYE", MessageType::BYE}, {"OK", MessageType::OK}, {"FAILED", MessageType::FAILED}, 
+	{"ERROR", MessageType::ERROR}, {"DATA", MessageType::DATA}, {"NONE", MessageType::NONE}};
+
+	msg.set_message_type(stringToArg[currArg]);
+
+
+	switch(stringToArg[currArg]) {
+
+		case MessageType::LOGIN:
+		case MessageType::PUSH:
+		case MessageType::CREATE:
+			s >> currArg;
+			msg.push_arg(currArg);
+			break; 
+		case MessageType::SET:
+		case MessageType::GET:
+			s >> currArg;
+			msg.push_arg(currArg);
+			currArg = "";
+			s >> currArg;
+			msg.push_arg(currArg);
+			break;
+		case MessageType::FAILED:
+			// encoded_msg += "FAILED " + msg.get_quoted_text();
+			break;
+		case MessageType::ERROR:
+			// encoded_msg += "ERROR " + msg.get_quoted_text();
+			break;
+		case MessageType::DATA:
+			// encoded_msg += "DATA " + msg.get_value();
+			break;
+		case MessageType::POP:
+		case MessageType::TOP:
+		case MessageType::ADD:
+		case MessageType::SUB:
+		case MessageType::MUL:
+		case MessageType::DIV:
+		case MessageType::BEGIN:
+		case MessageType::COMMIT:
+		case MessageType::BYE:
+		case MessageType::OK:
+		case MessageType::NONE:
+		break;
+	}
 }
