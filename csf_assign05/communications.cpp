d36fa03015c6acaf94716202aea68bcd8678be83
bbock27 <bradybock11@icloud.com>
2024-04-19 21:13:53 -0400

    remove debugging print statements

--- csf_assign05/communications.cpp
+++ csf_assign05/communications.cpp
@@ -11,16 +11,13 @@ void test() {
 int read_response(rio_t* fd, Message* msg) {
 	char buf[1024];
 	// buf[0] = '2';
-	std::cout << "reading" << std::endl;
-	ssize_t bytes_read = rio_readlineb(fd, buf, 2);
+	ssize_t bytes_read = rio_readlineb(fd, buf, 1024);
 	std::string encoded_msg(buf);
-	std::cout << encoded_msg << std::endl;
+	std::cout << encoded_msg;
 	if(bytes_read < 0) {
 		throw CommException("invalid write on read");
 	}
-	std::cout << "read line \n";
 	MessageSerialization::decode(encoded_msg, *msg);
-	std::cout << "decoded msg \n";
 
 
 	if(msg->get_message_type() == MessageType::FAILED || msg->get_message_type() == MessageType::ERROR) {
@@ -33,10 +30,9 @@ int send_message(int fd, Message* msg, std::string encoded_msg) {
 	MessageSerialization::encode(*msg, encoded_msg);
 
 	std::cout << encoded_msg;
-	int bytes_written = rio_writen(fd, &encoded_msg, encoded_msg.size());
+	int bytes_written = rio_writen(fd, encoded_msg.c_str(), strlen(encoded_msg.c_str()));
 	if(bytes_written != encoded_msg.size()) {
 		throw CommException("invalid write on write");
 	}
-	std:: cout << "done sending \n";
 	return bytes_written;
 }
