0c7ed11c99593969a573e784d5aa459ecf675f74
bbock27 <bradybock11@icloud.com>
2024-04-24 15:53:56 -0400

    server class mostly done. maybe... not quite sure

--- csf_assign05/server.cpp
+++ csf_assign05/server.cpp
@@ -1,15 +1,19 @@
+#include <exception>
 #include <iostream>
 #include <cassert>
 #include <memory>
+#include <vector>
 #include "csapp.h"
 #include "exceptions.h"
 #include "guard.h"
 #include "server.h"
+#include "table.h"
 
 Server::Server()
   // TODO: initialize member variables
 {
   // TODO: implement
+	tables = new std::vector<Table>();
 }
 
 Server::~Server()
@@ -19,21 +23,40 @@ Server::~Server()
 
 void Server::listen( const std::string &port )
 {
-  // TODO: implement
+  // TODO: implement - done??
+	server_fd = open_listenfd(port.c_str());
+	if (server_fd < 0) {
+		//add proper exception later 
+		throw CommException("Error opening server port");
+	}
 }
 
 void Server::server_loop()
 {
-  // TODO: implement
+  // TODO: implement - done?
 
-  // Note that your code to start a worker thread for a newly-connected
-  // client might look something like this:
-/*
+	int client_fd = Accept(server_fd, NULL, NULL);
+	if (client_fd < 0) {
+		throw CommException("Error accepting client connection");
+	}
+
   ClientConnection *client = new ClientConnection( this, client_fd );
   pthread_t thr_id;
-  if ( pthread_create( &thr_id, nullptr, client_worker, client ) != 0 )
-    log_error( "Could not create client thread" );
-*/
+  if ( pthread_create( &thr_id, nullptr, client_worker, client ) != 0 ){
+		log_error( "Could not create client thread" );
+	}
+    
+}
+
+void Server::create_table( const std::string &name ) {
+	//do we need to lock tables?
+	tables->push_back(Table(name));
+
+}
+Table *Server::find_table( const std::string &name ){
+	//search through tables vector and find find table with this name
+	//mutex?
+	//i think we dont need one bc its just looking and not changing anything but idk
 }
 
 
@@ -44,11 +67,11 @@ void *Server::client_worker( void *arg )
   // Assuming that your ClientConnection class has a member function
   // called chat_with_client(), your implementation might look something
   // like this:
-/*
+
   std::unique_ptr<ClientConnection> client( static_cast<ClientConnection *>( arg ) );
   client->chat_with_client();
   return nullptr;
-*/
+
 }
 
 void Server::log_error( const std::string &what )
--- csf_assign05/server.h
+++ csf_assign05/server.h
@@ -6,10 +6,16 @@
 #include <pthread.h>
 #include "table.h"
 #include "client_connection.h"
+#include "value_stack.h"
+#include <vector>
+
 
 class Server {
 private:
   // TODO: add member variables
+	
+	std::vector<Table>* tables;
+	int server_fd;
 
   // copy constructor and assignment operator are prohibited
   Server( const Server & );
@@ -28,12 +34,9 @@ public:
 
   // TODO: add member functions
 
-  // Some suggested member functions:
-/*
   void create_table( const std::string &name );
   Table *find_table( const std::string &name );
-  void log_error( const std::string &what );
-*/
+
 };
 
 
